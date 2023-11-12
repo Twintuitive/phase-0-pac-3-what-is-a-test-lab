06a5b8f39d48307ceb0333a9408b738c4e452666
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-11-12 15:06:00 -0500

    done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,25 +1,24 @@
-require ( './root.js' );
-
 const { name, height, message } = require("../index.js");
+const expect = require("chai").expect;
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      expect(name).to.equal("Susan");
     });
   });
 
   describe("Height", () => {
     it("is less than 40 and greater than 0", () => {
-      expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
+      expect(height).to.be.above(0);
+      expect(height).to.be.below(40);
     });
   });
 
   describe("Message", () => {
     it("gives the name and height", () => {
-      expect(message).toInclude(name);
-      expect(message).toInclude(height);
+      expect(message).to.include(name);
+      expect(message).to.include(height);
     });
   });
 });
